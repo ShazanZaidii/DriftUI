13d0f64f56f055343d20f77db82110c4193174f8
Shazan Zaidi <rit609009@gmail.com>
2026-01-15 13:41:44 +0530

    Updated to v81

--- driftui/build.gradle.kts
+++ driftui/build.gradle.kts
@@ -79,7 +79,7 @@ afterEvaluate {
 
                 groupId = "com.example"
                 artifactId = "driftui"
-                version = "0.1.78"
+                version = "0.1.81"
             }
         }
     }
